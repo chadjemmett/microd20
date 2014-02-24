582c0edb222e513c2eb28a207d5f571bab23fb49
Chad Jemmett <chad.jemmett@gmail.com>
2014-02-23 23:15:10 -0700

    add more stats.

--- micro.rb
+++ micro.rb
@@ -32,12 +32,23 @@ module MicroD20
   end
 
   class Player
-    attr_accessor :dex, :dex_bonus, :init
+    attr_accessor :dex, :dex_bonus, :str, :str_bonus, :wis, :wis_bonus, :init,
+    :atk, :ac, :hp, :level, :exp
 
     def initialize
       @dex = MicroD20::Dice.new(6).roll(3)
+      @str = MicroD20::Dice.new(6).roll(3)
+      @wis = MicroD20::Dice.new(6).roll(3)
       @dex_bonus = (@dex - 10)/2
+      @str_bonus = (@dex - 10)/2
+      @wis_bonus = (@dex - 10)/2
       @init = 0
+      @ac = 10 + @dex_bonus
+      @atk = @str_bonus
+#hp needs some tweaking. Want random number of at least 1
+      @hp = 8 + @str_bonus
+      @level = 1
+      @exp = 0
     end
   end
 end
